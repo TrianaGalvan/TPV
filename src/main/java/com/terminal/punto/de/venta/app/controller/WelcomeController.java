76bfe50dd66bcedaedbf412bcaf8feb3a2007590
TrianaGalvan <trianagalvan@gmail.com>
2018-11-27 10:38:56 -0600

    TPV Project template spring-boot with thymeleaf

---
+++ src/main/java/com/terminal/punto/de/venta/app/controller/WelcomeController.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018 Anzen Soluciones S.A. de C.V.
+ * Mexico D.F.
+ * All rights reserved.
+ *
+ * THIS SOFTWARE IS  CONFIDENTIAL INFORMATION PROPIETARY OF ANZEN SOLUCIONES.
+ * THIS INFORMATION SHOULD NOT BE DISCLOSED AND MAY ONLY BE USED IN ACCORDANCE THE TERMS DETERMINED BY THE COMPANY ITSELF.
+ */
+package com.terminal.punto.de.venta.app.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>TODO [Add comments of the class]</p>
+ * 
+ * @author Triana Prieto {@literal <triana.prieto@anzen.com.mx>}
+ * @version tpv
+ * @since tpv
+ * @category
+ */
+@Controller
+public class WelcomeController {
+    
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+}
